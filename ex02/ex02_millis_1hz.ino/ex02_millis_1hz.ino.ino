9a73172bfbcf72f258d497ea8ab99d05851e1fd5
lxy <1830425318@qq.com>
2026-06-19 19:49:25 +0800

    ex02: 补充millis非阻塞原理详细注释，对比delay阻塞代码优缺点

--- ex02/ex02_millis_1hz.ino/ex02_millis_1hz.ino.ino
+++ ex02/ex02_millis_1hz.ino/ex02_millis_1hz.ino.ino
@@ -1,6 +1,8 @@
-// ex02 使用millis实现无阻塞1Hz闪烁，周期2000ms
+// ex02 使用millis实现无阻塞LED闪烁，对比delay阻塞式代码
 const int ledPin = 2;
+// 记录上次翻转LED的系统时间
 unsigned long lastTime = 0;
+// 闪烁周期2000ms，亮2秒灭2秒
 const unsigned long interval = 2000;
 bool ledStatus = LOW;
 
@@ -10,8 +12,9 @@ void setup() {
 }
 
 void loop() {
+  // 获取当前系统运行毫秒数
   unsigned long currentTime = millis();
-  // 时间差达到1000ms时翻转LED
+  // 判断间隔是否达到设定周期
   if (currentTime - lastTime >= interval) {
     lastTime = currentTime;
     ledStatus = !ledStatus;
@@ -19,4 +22,5 @@ void loop() {
     Serial.print("LED状态：");
     Serial.println(ledStatus ? "亮" : "灭");
   }
+  // 优势：loop内其他代码不会被延时阻塞，可同时执行多任务
 }
