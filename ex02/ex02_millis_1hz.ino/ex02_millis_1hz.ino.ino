15a58748ec28e574cf43360dabc71264f3903408
lxy <1830425318@qq.com>
2026-06-19 19:47:12 +0800

    ex02: 使用millis函数实现无阻塞1Hz周期LED闪烁，串口打印开关状态

---
+++ ex02/ex02_millis_1hz.ino/ex02_millis_1hz.ino.ino
@@ -0,0 +1,22 @@
+// ex02 使用millis实现无阻塞1Hz闪烁，周期1000ms
+const int ledPin = 2;
+unsigned long lastTime = 0;
+const unsigned long interval = 1000;
+bool ledStatus = LOW;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentTime = millis();
+  // 时间差达到1000ms时翻转LED
+  if (currentTime - lastTime >= interval) {
+    lastTime = currentTime;
+    ledStatus = !ledStatus;
+    digitalWrite(ledPin, ledStatus);
+    Serial.print("LED状态：");
+    Serial.println(ledStatus ? "亮" : "灭");
+  }
+}
